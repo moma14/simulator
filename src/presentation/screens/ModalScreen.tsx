b20eb23db98a67ff718a4516c475517d0f41cef3
moma14 <151698538+moma14@users.noreply.github.com>
2025-02-28 17:55:31 -0600

    creación de una lista dentro del modal

--- src/presentation/screens/ModalScreen.tsx
+++ src/presentation/screens/ModalScreen.tsx
@@ -1,13 +1,23 @@
 import {View, Modal, Text, Pressable, Image, StyleSheet} from "react-native"
 import React, { useState } from "react"
+import { FlatList } from "react-native-gesture-handler";
+
+const ListaModal=[
+    {id:"1", text:'Jose Cob'},
+    {id:"2", text:'Carlos Paz'},
+    {id:"3", text:'David Trejo'},
+    {id:"4", text:'Tony Herrera'},
+]
 
 const ModalScreen=()=>{
-    //
+    //se definen las variableel estado para el modal
+    //para cuando el modal se despliegue y cuando no 
     const [modalVisible, setModalVisible]=useState(false);
+
 
     return(
 
-        <View style={styles.ModalScreen}>
+        <View style={styles.ModalScreen} >
             <Text style={ styles.text}> Esta es la vista del modal</Text>
             <Pressable style={styles.buttonPressable} onPress={()=>setModalVisible(true)}>
                 <Text style={styles.buttonText}>Despliega el modal</Text>
@@ -23,10 +33,18 @@ const ModalScreen=()=>{
                 <Pressable style={styles.buttonPressable} onPress={()=>setModalVisible(false)}>
                     <Text style={styles.buttonText}>Despliega el modal</Text>
                 </Pressable>
-
+                <Text style={styles.text}>Este texto va adentro del modal</Text>
+                <FlatList
+                    data={ListaModal}
+                    keyExtractor={(item)=>item.id}
+                    renderItem={({item})=>(
+                        <View style={styles.textContainer}>
+                            <Text style={styles.text}>{item.text}</Text>
+                        </View>
+                    )}
+                    contentContainerStyle={styles.contentContainer}
+                ></FlatList>
             </Modal>
-
-
         </View>
     );
 };
@@ -37,10 +55,23 @@ const styles=StyleSheet.create({
         justifyContent:'center',//centra el contenido verticalmente
         alignItems:'center',//lo centra
     },
+    contentContainer:{
+        flexGrow:1,//permite que la lista crezca y se mantenga centrada
+        justifyContent:'center',//centra los elementos dentro del FlatList
+        alignItems:'center',
+    },
     text:{
         fontFamily:'16',
         color:'#333',
         marginBottom:10,
+        textAlign:'center',
+        marginTop:15,
+    },
+    textContainer:{
+        fontFamily:'16',
+        color:'#333',
+        marginBottom:10,
+        textAlign:'center',
     },
     buttonPressable:{
         alignItems:'center',
